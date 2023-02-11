89dc2a5f2de5315da763a0335e3ff613631b3316
Abdur Rahman <arwithlpd@gmail.com>
2023-02-11 11:37:09 +0600

    smart Pointer to Object class

--- Advanced/SmartPointer/main.cpp
+++ Advanced/SmartPointer/main.cpp
@@ -2,24 +2,53 @@
 
 using namespace std;
 
-// Smart Pointer to object Class example:
+// Smart unique pointer class template:
 
+template<typename T>
 class SmartPtr {
-    int *ptr; // Actual pointer
+private:
+    T *ptr;
 public:
-    // Constructor: Refer https://www.geeksforgeeks.org/g-fact-93/ for details
-    explicit SmartPtr(int *p = NULL) { ptr = p; }
+    SmartPtr(T *p = nullptr) : ptr(p) {}
 
-    // Destructor
-    ~SmartPtr() { delete (ptr); }
+    ~SmartPtr() { delete ptr; }
 
-    // Overloading dereferncing operator
-    int &operator*() { return *ptr; }
+    // Copy constructor
+    SmartPtr(SmartPtr &sp) = delete;
+
+    // Move constructor
+    SmartPtr(SmartPtr &&sp) noexcept {
+        ptr = sp.ptr;
+        sp.ptr = nullptr;
+    }
+
+    // Copy assignment
+    SmartPtr &operator=(SmartPtr &sp) = delete;
+
+    // Move assignment
+    SmartPtr &operator=(SmartPtr &&sp) noexcept {
+        if (this != &sp) {
+            delete ptr;
+            ptr = sp.ptr;
+            sp.ptr = nullptr;
+        }
+        return *this;
+    }
+
+    T &operator*() { return *ptr; }
+
+    T *operator->() { return ptr; }
+
 };
 
 int main() {
-    SmartPtr ptr(new int());
-    *ptr = 20;
-    cout << *ptr;
+    SmartPtr<int> sp1(new int);
+    *sp1 = 10;
+    cout << *sp1 << endl;
+
+    SmartPtr<int> sp2 = move(sp1);
+    cout << *sp2 << endl;
+
     return 0;
 }
+
